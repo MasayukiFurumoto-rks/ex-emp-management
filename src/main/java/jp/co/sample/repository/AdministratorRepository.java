49f14bb4551729dc1a5823e7bae46d94c74e91d3
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-02 14:50:53 +0900

    レポジトリクラス　AdministratorRepository.javaを作成しました。

---
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -0,0 +1,41 @@
+package jp.co.sample.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Administrator;
+
+@Repository
+public class AdministratorRepository {
+//	RowMapperを一行で記載する場合は以下。
+//	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER 
+//							= new BeanPropertyRowMapper<>(Administrator.class);
+
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs,i)->{
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAddress(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("password"));
+		return administrator;
+	};
+	
+	private static final String TABLE_NAME = "administrators";
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template ;
+
+
+	public void insert(Administrator administrator) {
+		String sql = "INSERT INTO " + TABLE_NAME + " (name,mail_address,password) VALUES (:name,:mailAddress,:password);";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+		template.update(sql, param);
+	}
+	
+}
