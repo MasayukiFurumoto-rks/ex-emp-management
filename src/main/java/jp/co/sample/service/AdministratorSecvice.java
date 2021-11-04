7b4308edd44b9d150b28836b216b70b4784a6866
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-04 10:09:42 +0900

    AdministratorRepositoryにinsertメソッドを追加しました。

--- src/main/java/jp/co/sample/service/AdministratorSecvice.java
+++ src/main/java/jp/co/sample/service/AdministratorSecvice.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
+/**
+ *　管理者情報に関係する処理を実行するサービスクラスです。<br>
+ * @author cyjoh
+ */
 @Service
 @Transactional
 public class AdministratorSecvice {
@@ -13,4 +18,12 @@ public class AdministratorSecvice {
 	@Autowired
 	private AdministratorRepository repository;
 
+	/**
+	 * 管理者情報をレポジトリクラスに渡し、追加指示を行うメソッドです。
+	 * @param administrator 外部で指定された管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		repository.insert(administrator);
+	}
+	
 }
