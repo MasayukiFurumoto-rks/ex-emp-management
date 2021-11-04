e5fc8cc67286f7f57f353916cb735c9442e67a7c
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-04 10:19:20 +0900

    AdministratorControllerに管理者追加処理を実行するためのinsertメソッドを追加しました。

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,16 +1,20 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorSecvice;
 
 /**
- * @author cyjoh
  * 管理者アカウントに関する機能を担当するコントローラークラスです。<br>
+ * 
+ * @author cyjoh
  */
 @Controller
 @RequestMapping("/")
@@ -20,7 +24,7 @@ public class AdministratorController {
 	private AdministratorSecvice service;
 
 	/**
-	 * 管理者を追加する用の入力画面に使うフォームクラスをインスタンス化してリクエストスコープに格納するメソッドです。
+	 * 管理者を追加する用の入力画面に使うフォームクラスをインスタンス化してリクエストスコープに格納するメソッドです。<br>
 	 * 
 	 * @return インスタンス化したInsertAdministratorForm
 	 */
@@ -30,14 +34,27 @@ public class AdministratorController {
 	}
 
 	/**
-	 * 管理者を追加する画面を表示するためのメソッドです。
-	 * @return　管理者アカウント追加用入力画面を返します。
+	 * 管理者を追加する画面を表示するためのメソッドです。<br>
+	 * 
+	 * @return 管理者アカウント追加用入力画面を返します。
 	 */
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
 	}
 
+	/**
+	 * 管理者を追加する指示を行うメソッドです。<br>
+	 * 
+	 * @param form 管理者登録の情報入力用画面から、InsertAdministratorFormが受け取った内容です。
+	 * @return　ログイン画面を表示するためのURLを返します。
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		service.insert(administrator);
+		return "/";
+	}
+
 }
-
-
