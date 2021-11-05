37d805631bd84c0ace2606db6f553dec676ae9a8
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-05 09:12:46 +0900

    InsertAdministratorForm.javaで入力エラーチェックを行うようにしました。

--- src/main/java/jp/co/sample/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -1,17 +1,24 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author cyjoh 管理者を追加する際に、必要な情報を受け取るためのフォームクラスです。<br>
  * 
  */
 public class InsertAdministratorForm {
 	/** 氏名 */
+	@NotBlank(message="名前は必須です。")
 	private String name;
 
 	/** メールアドレス */
+	@NotBlank(message="メールアドレスは必須です。")
+	@Email(message="メールアドレスは正しい形式で入力してください。")
 	private String mailAddress;
 
 	/** パスワード */
+	@NotBlank(message="パスワードは必須です。")
 	private String password;
 
 	public String getName() {
