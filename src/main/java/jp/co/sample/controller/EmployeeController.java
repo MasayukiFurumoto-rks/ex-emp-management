7f459112dde579f5a2540e0918bb7cf1212e70cd
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-05 09:31:34 +0900

    従業員情報（扶養人数）更新の際に入力値エラーチェックを行うようにしました。

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -62,7 +64,12 @@ public class EmployeeController {
 	 * @return 従業員一覧の画面にリダイレクトさせるURL
 	 */
 	@RequestMapping("/update")
-	public String update(UpdateEmployeeForm form) {
+	public String update(@Validated UpdateEmployeeForm form,BindingResult result,Model model) {
+		
+		if(result.hasErrors()) {
+			return showDetail(form.getId(),model);
+		}
+		
 		Employee employee = new Employee();
 		//formから送られてきたidをもとに、データベースから従業員情報を取得
 		employee = service.showDetail(Integer.parseInt(form.getId()));
--- src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -1,5 +1,8 @@
 package jp.co.sample.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 /**
  * 従業員情報を更新するための情報を受け取るためのフォームクラスです。<br>
  * 
@@ -11,6 +14,8 @@ public class UpdateEmployeeForm {
 	private String id;
 
 	/** 扶養人数 */
+	@NotBlank(message="扶養人数を入力してください。")
+	@Pattern(regexp="^[0-9]+$",message="扶養人数は1桁以上の半角数字で入力してください。")
 	private String dependentsCount;
 
 	public String getId() {
